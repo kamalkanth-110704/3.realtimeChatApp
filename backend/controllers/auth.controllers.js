c3f99f6d5c4231603c620810137bb61426498cbc
kamalkanth-110704 <kamalkanthsilla@gmail.com>
2025-06-09 12:11:25 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -56,7 +56,7 @@ export const login=async (req,res)=>{
  res.cookie("token",token,{
      httpOnly:true,
      maxAge:7*24*60*60*1000,
-     sameSite:"NULL",
+     sameSite:"None",
      secure:true
     })
  
